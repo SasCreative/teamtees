1ea570d524a1c591b38f147d873c4892732ff12c
Buddhi Prasan <buddhiprasan12@gmail.com>
2020-02-03 14:40:55 +0530

    index about

--- wp-content/themes/teamtees/front-page.php
+++ wp-content/themes/teamtees/front-page.php
@@ -4,4 +4,5 @@
 <?php get_template_part('page-templates/index/index', 'main_product'); ?>
 <?php get_template_part('page-templates/index/index', 'call'); ?>
 <?php get_template_part('page-templates/index/index', 'steps'); ?>
+<?php get_template_part('page-templates/index/index', 'about'); ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/teamtees/page-templates/index/index-about.php
@@ -0,0 +1,21 @@
+<section id="index_about">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <iframe src="https://www.youtube.com/embed/zLkNgkzx-wI" frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+            <div class="col-md-5">
+                <div class="index_about_right">
+                    <h2>who we are</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Quis ipsum suspendisse </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                    <a href="">Read More</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
