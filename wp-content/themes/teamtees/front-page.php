5358de9ebb6cd717316ad557413ff1c23c668313
Buddhi Prasan <buddhiprasan12@gmail.com>
2020-02-03 13:43:26 +0530

    tab section

--- wp-content/themes/teamtees/front-page.php
+++ wp-content/themes/teamtees/front-page.php
@@ -3,4 +3,5 @@
 <?php get_template_part('page-templates/index/index', 'main_icon'); ?>
 <?php get_template_part('page-templates/index/index', 'main_product'); ?>
 <?php get_template_part('page-templates/index/index', 'call'); ?>
+<?php get_template_part('page-templates/index/index', 'steps'); ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/teamtees/page-templates/index/index-steps.php
@@ -0,0 +1,78 @@
+<section id="index_steps">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="row justify-content-end">
+                    <div class="col-md-6">
+                        <h2>design your own shirt in just a few steps
+                        </h2>
+
+                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="active" id="pills-home-tab" data-toggle="pill" href="#pills-home"
+                                    role="tab" aria-controls="pills-home" aria-selected="true">step 1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="" id="pills-profile-tab" data-toggle="pill" href="#pills-profile"
+                                    role="tab" aria-controls="pills-profile" aria-selected="false">step 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="" id="pills-contact-tab" data-toggle="pill" href="#pills-contact"
+                                    role="tab" aria-controls="pills-contact" aria-selected="false">step 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                        aria-labelledby="pills-home-tab">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img class="img-fluid mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/img/tshirt-line.png" alt="">
+                            </div>
+                            <div class="col-md-6">
+                                <h3>
+                                    choose our product
+                                </h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum</p>
+                                <a href="#">Tshirt</a> | <a href="#">
+                                    Caps</a> | <a href="#">hoodies</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                        <div class="row">
+                            <div class="col-md-6"><img class="img-fluid mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/img/tshirt-line.png" alt="">
+                            </div>
+                            <div class="col-md-6">
+                                <h3>
+                                    choose our product
+                                </h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum</p>
+                                <a href="#">Tshirt</a> | <a href="#">
+                                    Caps</a> | <a href="#">hoodies</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                        <div class="row">
+                            <div class="col-md-6"><img class="img-fluid mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/img/tshirt-line.png" alt="">
+                            </div>
+                            <div class="col-md-6">
+                                <h3>
+                                    choose our product
+                                </h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum</p>
+                                <a href="#">Tshirt</a> | <a href="#">
+                                    Caps</a> | <a href="#">hoodies</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
